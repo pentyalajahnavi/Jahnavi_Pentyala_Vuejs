7f0e46e42d97451c2c2170a443ebf6e0f63d01fa
pentyalajahnavi <pentyala.jahnavi28@gmail.com>
2024-04-27 18:10:54 -0400

    Vue js commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--Step 3 use the component-->
+  <AppHeader header="Exam Schedule Tracker" />
+  <ExamInfoBox :examDetails="examDetails" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Step: 1 Import the components
+import AppHeader from './components/AppHeader.vue';
+import ExamInfoBox from './components/ExamInfoBox.vue';
 
-export default {
+export default{
   name: 'App',
-  components: {
-    HelloWorld
+  // Step 2: Register the components
+  components:{
+    AppHeader,
+    ExamInfoBox
+  },
+  data(){
+    return {
+      examDetails: []
+    }
+  },
+  methods:{
+    //promises
+    async fetchExamDetails(){
+      const res = await fetch('https://exam-wbnl.onrender.com/api');
+      const data = await res.json();
+      console.log(data);
+      return data[0].ExamData;
+    }
+  },
+  async created(){
+      this.examDetails = await this.fetchExamDetails();
   }
+
+  // data() => this.data examDetails
+  // method => this.method => fetchExamDetails
+  // life cycle hooks created() => call our method here
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  /* font-family: 'Montserrat', sans-serif; */
+}
+
+.container {
+  max-width: 400px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+div{
+  margin-bottom: 1em;
 }
 </style>
